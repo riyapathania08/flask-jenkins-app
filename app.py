b40629117254c217b3e5873069d43d206c63166b
Riya Pathania <167676959+riyapathania08@users.noreply.github.com>
2025-10-30 14:15:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,33 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "🚀 Hello from Flask app deployed via Jenkins + Docker!"
+def hello():
+    return """
+    <html>
+        <head>
+            <title>Flask CI/CD App</title>
+            <style>
+                body {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
+                    background-color: #000;   /* Black background */
+                    color: #fff;              /* White text */
+                    font-family: 'Arial', sans-serif;
+                    margin: 0;
+                }
+                h1 {
+                    font-size: 24px;
+                    text-align: center;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>🌸 Hello from Flask app deployed via Jenkins + Docker!</h1>
+        </body>
+    </html>
+    """
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
